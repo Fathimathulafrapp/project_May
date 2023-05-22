151e19f17e3bb8e6dc0494a0f3156466eb49b34a
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-22 15:05:58 +0530

    commit

---
+++ bin/variable.dart
@@ -0,0 +1,12 @@
+void main(){
+  String name="afra";
+  int age=20;
+  double mark=5.8;
+  String course="flutter";
+  print("my name is  ${name}");
+  print("age: $age");
+  print("mark is $mark");
+  print("course is $course");
+
+
+}
