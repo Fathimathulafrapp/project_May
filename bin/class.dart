492a1ffaf416c4f955e41e5399c2c0d26c922ed5
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-22 15:55:08 +0530

    commit

--- bin/class.dart
+++ bin/class.dart
@@ -1,15 +1,42 @@
 class my_class{
-  String name="Afra";
-  int age= 20;
-  double mark= 56.7;
+  String  ?name;
+  int ?age;
+  double ?mark;
   static String course ="flutter";
 }
 void main()
 {
   my_class obj=my_class();
-  print(" My Name ${obj.name}");
-  print(" My age ${obj.age}");
-  print(" My mark ${obj.mark}");
+  print("student 1 details");
+  print(" My Name ${obj.name="afra"}");
+  print(" My age ${obj.age=20}");
+  print(" My mark ${obj.mark=56.8}");
   print("My course ${my_class.course}" );
+  print("");
+
+  my_class obj1=my_class();
+  print("student2 details");
+  print(" My Name ${obj.name="anu"}");
+  print(" My age ${obj.age=19}");
+  print(" My mark ${obj.mark=57.8}");
+  print("My course ${my_class.course}" );
+  print("");
+
+  my_class obj2=my_class();
+  print("student3 details");
+  print(" My Name ${obj.name="Shammu"}");
+  print(" My age ${obj.age=25}");
+  print(" My mark ${obj.mark=49.8}");
+  print("My course ${my_class.course}" );
+  print("");
+
+
+  my_class obj3=my_class();
+  print("student4 details");
+  print(" My Name ${obj.name="dana"}");
+  print(" My age ${obj.age=25}");
+  print(" My mark ${obj.mark=34.8}");
+  print("My course ${my_class.course}" );
+  print("");
 
 }
