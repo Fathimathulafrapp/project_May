2056ec995b974f0377f1771d65a5381289f11601
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-23 15:28:59 +0530

    commit

---
+++ bin/function inside class.dart
@@ -0,0 +1,40 @@
+import 'class.dart';
+
+class Myclass {
+  void add() {
+    int a = 21,
+        b = 9,
+        sum = a + b;
+    print("$a + $b =$sum");
+  }
+
+  void sub() {
+    int a = 45,
+        b = 42,
+        sub = a - b;
+    print("$a -$b =$sub");
+  }
+
+  void mul() {
+    int a = 4,
+        b = 2,
+        mul = b * a;
+    print("$a * $b = $mul");
+  }
+
+  void div() {
+    int a = 15,
+        b = 4,
+        div = a ~/ b;
+    print("$a ~/ $b = $div");
+  }
+}
+void main(){
+  Myclass obj = Myclass();
+  obj.add();
+  obj.sub();
+  obj.mul();
+  obj.div();
+
+
+}
