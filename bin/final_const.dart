715047a7409a17456cf4c663a84f669c8116f9a3
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-23 15:55:51 +0530

    commit

---
+++ bin/final_const.dart
@@ -0,0 +1,12 @@
+void main()
+{
+  String name ="shammu";
+  int age=25;
+  final String course;
+  course= "flutter";
+  const String batch="python";
+  print(name);
+  print(age);
+  print(course);
+  print(batch);
+}
