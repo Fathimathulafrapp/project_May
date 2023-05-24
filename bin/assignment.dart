df7f68ce89055cb83862138f235e75eb9f53db9c
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-24 15:07:13 +0530

    commit

---
+++ bin/assignment.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  dynamic x=40,y=30;
+  print("x=y ${x=y}" );
+  print("x+=y ${x+=y}" );
+  print("x-=y ${x-=y}" );
+  print("x*=y ${x*=y}" );
+  print("x/=y ${x/=y}" );
+  print("x~/=y ${x~/=y}" );
+  print("x%=y ${x%=y}" );
+}
