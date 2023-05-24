cbdb8b5dd24491897a7880e86e94d841f953a3d4
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-24 14:51:01 +0530

    commit

---
+++ bin/arithemetic.dart
@@ -0,0 +1,15 @@
+void main(){
+  var a=40 ;
+  var b =5;
+  print("a+b =${a+b}");
+  print("a-b =${a-b}");
+  print("a*b =${a*b}");
+  print("a/b =${a/b}");
+  print("a~/b =${a~/b}");
+  print("a%b =${a%b}");
+  print("-(a+b)=${-(a+b)}");
+
+
+
+
+}
