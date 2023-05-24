cddfeab80760ca187530621ebc4f3d6093cb0131
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-05-24 15:52:49 +0530

    commit

---
+++ bin/logicalop.dart
@@ -0,0 +1,9 @@
+void main()
+{
+String username="abcd";
+int password=45678;
+print(username=="abcd" && password==45678);
+print(username=="abcd" || password==4567800);
+print(!(username=="abcd" || password==45678));
+
+}
---
+++ bin/typetestop.dart
@@ -0,0 +1,9 @@
+void main()
+{
+  var name ="shanu";
+  int age=18;
+  print(name is int);
+  print(age is! int);
+  print(name is String);
+
+}
