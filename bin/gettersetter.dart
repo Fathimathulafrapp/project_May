1b5dd1543e02d5a12bf1316a2342956775596b05
Fathimathul Afra <fathimathulafra66@gmail.com>
2023-06-17 11:58:31 +0530

    commit

---
+++ bin/gettersetter.dart
@@ -0,0 +1,41 @@
+class myclass4{
+  late String name;
+  late int age;
+  late String course;
+  late double mark;
+
+  String get gtname{
+    return name;
+
+  }
+  int get gtage{
+    return age;
+
+  }
+  String get gtcourse{
+    return course;
+
+  }
+  double get gtmark{
+    return mark;
+
+  }
+  set setname(String name){
+    this.name=name;
+
+  }
+
+  set setage(int age){
+    this.age=age;
+
+  }
+  set setcourse(String course){
+    this.course=course;
+
+  }
+  set setmark(double mark){
+    this.mark=mark;
+
+  }
+  }
+
---
+++ bin/getuser.dart
@@ -0,0 +1,14 @@
+import 'gettersetter.dart';
+
+void main()
+{
+  var obj =myclass4();
+  obj.name="afra";
+  obj.age=20;
+  obj.course="flutter";
+  obj.mark=60.8;
+  print(" name is ${obj.gtname}");
+  print(" age is ${obj.gtage}");
+  print(" course is ${obj.gtcourse}");
+  print(" mark is ${obj.gtmark}");
+}
